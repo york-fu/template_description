caac599cee6fd06b9a82e25dae517627e81589c0
york-fu <york-fu@outlook.com>
2025-11-23 18:04:46 +0800

    fix: viz.launch

--- launch/viz.launch.py
+++ launch/viz.launch.py
@@ -8,24 +8,17 @@ import os
 def generate_launch_description():
     pkg_share = FindPackageShare("robot_description_template").find("robot_description_template")
     urdf_file = os.path.join(pkg_share, "urdf", "robot_description_template.urdf")
-    rviz_config = os.path.join(pkg_share, "rviz", "display.rviz")
+    rviz_config = os.path.join(pkg_share, "rviz", "viz.rviz")
 
     return LaunchDescription([
-        Node(
-            package="joint_state_publisher_gui",
-            executable="joint_state_publisher_gui",
-            name="joint_state_publisher_gui",
-            output="screen",
-            parameters=[{
-                'source_list': ['rviz/joint_state'],
-                'rate': 60,
-            }]
-        ),
         Node(
             package="robot_state_publisher",
             executable="robot_state_publisher",
             output="screen",
-            parameters=[{"robot_description": open(urdf_file).read()}]
+            parameters=[{
+                "robot_description": open(urdf_file).read(),
+                'publish_frequency': 60.0,
+            }]
         ),
         Node(
             package="rviz2",
